a05e7300babca9de2f4e3fa46272502c928f0290
REMO-ios <remo-ios@REMO-iosui-MacBookPro.local>
2022-12-15 10:20:26 +0900

    chore: Add ItemSeparatorComponent to PersonList

--- components/PersonList.tsx
+++ components/PersonList.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FlatList } from 'react-native';
+import { FlatList, View } from 'react-native';
 import { PersonType } from '../utils/types';
 import Person from './Person';
 import personData from '../utils/personData.json';
@@ -11,13 +11,22 @@ interface Props {
 function PersonList({ isRefreshing, onRefresh }: Props) {
   return (
     <FlatList
+      ItemSeparatorComponent={() => (
+        <View
+          style={{
+            width: '100%',
+            height: 1,
+            backgroundColor: 'yellow',
+            marginVertical: 10,
+          }}></View>
+      )}
       onRefresh={onRefresh}
       refreshing={isRefreshing}
       style={{ width: '100%' }}
       data={personData as PersonType[]}
       renderItem={props => <Person {...props} />}
       keyExtractor={item => item.name}
-      showsHorizontalScrollIndicator={false}
+      showsVerticalScrollIndicator={false}
     />
   );
 }
