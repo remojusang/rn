d7929bcb24fb20fd48974a9cf67a4fa95f360719
REMO-ios <remo-ios@REMO-iosui-MacBookPro.local>
2022-12-19 18:30:41 +0900

    chore: Add ErrorBoundary

--- components/ErrorBoundary.tsx
+++ components/ErrorBoundary.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
-export default class ErrorBoundary extends React.Component {
-  constructor(props) {
+export default class ErrorBoundary extends React.Component<
+  { children: any },
+  {
+    hasError: boolean;
+  }
+> {
+  constructor(props: any) {
     super(props);
     this.state = { hasError: false };
   }
 
-  static getDerivedStateFromError(error) {
+  static getDerivedStateFromError() {
     // Update state so the next render will show the fallback UI.
     return { hasError: true };
   }
 
-  componentDidCatch(error, errorInfo) {
+  componentDidCatch() {
     // You can also log the error to an error reporting service
   }
 
